af9a635977e271cd3781efaa121eca142346e0f5
kalimuthu949 <kalimuthu210@gmail.com>
2025-05-17 14:41:15 +0530

    test

--- Tabs.tsx
+++ Tabs.tsx
@@ -8,7 +8,7 @@ import { set } from "@microsoft/sp-lodash-subset";
 const Tabs: React.FC = () => {
   const [activeTab, setActiveTab] = useState(0);
   const [tabsData, setTabsData] = useState([]);
-  console.log(tabsData);
+  console.log("test");
   useEffect(() => {
     const fetchData = async () => {
       let Tabs=[];
